c013363187d4b98119fbf4f79b61e15bd7f73add
AndreyDuda <andreyduda88@gmail.com>
2019-03-19 19:58:08 +0200

    loop wp

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -1,5 +1,27 @@
 <?php get_header(); ?>
 
-<h1>Hello, world!</h1>
+<div class="container">
+    <div class="row">
+<?php if(have_posts()): ?>
+    <?php while(have_posts()): ?>
+        <?php the_post(); ?>
+        <div class="col-md-12">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title"><?php the_title() ?></h5>
+                    <p class="card-text"><?php the_excerpt() ?></p>
+                    <a href="<?php the_permalink() ?>" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    <?php endwhile; ?>
+
+<?php else: ?>
+    <p> Постов нет ...</p>
+<?php endif; ?>
 
 <?php get_footer(); ?>
+
+</div>
+</div>
