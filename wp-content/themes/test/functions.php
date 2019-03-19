3703ee4651ea77a7fc900bd2ea3885e6735ad3ee
AndreyDuda <andreyduda88@gmail.com>
2019-03-19 18:45:59 +0200

    include scripts and style

--- wp-content/themes/test/functions.php
+++ wp-content/themes/test/functions.php
@@ -1,1 +1,18 @@
 <?php
+
+
+function test_scripts() {
+	wp_enqueue_style('test-bootstrapcss', get_template_directory_uri() . '/assets/bootstrap/css/bootstrap.min.css');
+	wp_enqueue_style('test-style', get_template_directory_uri() . '/style.css');
+
+	add_action('wp_footer', function (){
+		wp_deregister_script('jquery');
+		wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js');
+
+		/*wp_enqueue_script('jquery');*/
+		wp_enqueue_script('test-popper', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js', array('jquery'));
+		wp_enqueue_script('test-bootstrapjs', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.min.js');
+	});
+}
+
+add_action('wp_enqueue_scripts', 'test_scripts');
